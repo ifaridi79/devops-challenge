3924ac2682cfbc822ba505ef5fefee90392f01e6
Imran Faridi <imran.faridi@icloud.com>
2023-05-04 18:16:33 -0400

    rbac

--- infrastructure/aks-cluster.tf
+++ infrastructure/aks-cluster.tf
@@ -33,15 +33,6 @@ resource "azurerm_kubernetes_cluster" "aks" {
     os_disk_size_gb = 30
   }
 
-  service_principal {
-    client_id     = var.ARM_CLIENT_ID
-    client_secret = var.ARM_CLIENT_SECRET
-  }
-
-  role_based_access_control {
-    enabled = true
-  }
-
   tags = {
     environment = "Liatrio-Demo"
   }
